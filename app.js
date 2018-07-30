a2b300ae9f143a30dd439dea582682b2939dc647
bschue <bschuess3@gmail.com>
2018-07-30 14:18:09 -0400

    removed unused material

--- app.js
+++ app.js
@@ -22,63 +22,14 @@ app.get('/', (req, res) => {
             })
             console.log(photosArr)
         res.render("homePage", {photosArr}
-            // `<!DOCTYPE html>
-            // <html lang="en">
-            // <head>
-            // <meta charset="UTF-8">
-            // <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            // <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            // <link rel="stylesheet" href="style.css">
-            // <title>Kenziegram</title>
-            // </head>
-            // <body>
-            // <form id="user-create-form" action="/upload" method="post" enctype="multipart/form-data">
-
-            // <fieldset id="yourInfo">
-
-            // <legend>Welcome To Kenziegram!</legend>
-
-            // <label for="myFile">Upload an image:</label>
-            // <input type="file" id="myFile" name="myFile" />
-            // <br>
-            // <br>
-
-            // <button type="submit">Submit</button>
-            // <button type="reset">Reset</button>
-
-            // </fieldset>
-            // </form>
-            // <div id="photosDiv">
-            // ${joinedPhotosArr}
-            // </div>
-            // </body>
-            // </html>`
         )
     });
 })
 
 app.post('/upload', upload.single('myFile'), function (request, response, next) {
-    // request.file is the `myFile` file
-    // request.body will hold the text fields, if there were any
     fileToUpload = request.file.filename
     photosArr.push(fileToUpload)
     response.render("uploadPage", {fileToUpload}
-    // `<!DOCTYPE html>
-    // <html lang="en">
-    // <head>
-    // <meta charset="UTF-8">
-    // <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    // <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    // <title>Kenziegram</title>
-    // </head>
-    // <body>
-    // <img src="/uploads/${request.file.filename}">
-    // <p>You successfully uploaded your image</p>
-    // <a href="/">
-    // <button id="homepage" name="homepage" type="submit">Return To Homepage</button>
-    // </a>
-    // </body>
-    // </html>`
     )
 })
 
